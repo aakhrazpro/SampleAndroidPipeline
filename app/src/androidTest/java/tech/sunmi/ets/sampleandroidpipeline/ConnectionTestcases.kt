a62268cb3fb1c3f1ce05733bc110e7b8d8843854
aahmed <ahmed.akhraz@gmail.com>
2022-03-05 08:15:15 +0100

    Update with some dummy instrumented testcases

--- app/src/androidTest/java/tech/sunmi/ets/sampleandroidpipeline/ConnectionTestcases.kt
+++ app/src/androidTest/java/tech/sunmi/ets/sampleandroidpipeline/ConnectionTestcases.kt
@@ -14,11 +14,25 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ConnectionTestcases {
     @Test
-    fun useAppContext() {
+    fun test_if_connection_is_OK_With_Device() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("tech.sunmi.ets.sampleandroidpipeline", appContext.packageName)
+    }
+    @Test
+    fun test_if_SDK1_can_be_connected() {
+        assertTrue("SDK1 service is connected",true)
+    }
+
+    @Test
+    fun test_if_SDK2_can_be_connected() {
+        assertTrue("SDK2 service is connected",false)
+    }
+
+    @Test
+    fun test_if_SDK3L_can_be_connected() {
+        assertTrue("SDK3 service is connected",true)
     }
 }
---
+++ app/src/androidTest/java/tech/sunmi/ets/sampleandroidpipeline/InitialTestcases.kt
@@ -0,0 +1,32 @@
+package tech.sunmi.ets.sampleandroidpipeline
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class InitialTestcases {
+
+    @Test
+    fun initial_1_Testcase() {
+        Assert.assertTrue("Test case 1 is ok", true)
+    }
+    @Test
+    fun initial_2_Testcase() {
+        Assert.assertTrue("Test case 2 is ok", true)
+    }
+    @Test
+    fun initial_3_Testcase() {
+        Assert.assertTrue("Test case 3 is ok", true)
+    }
+    @Test
+    fun initial_4_Testcase() {
+        Assert.assertTrue("Test case 4 is ok", true)
+    }
+    @Test
+    fun initial_5_Testcase() {
+        Assert.assertTrue("Test case 5 is ok", true)
+    }
+}
